d1012afd3b7d1acf89fcdde7a3c1a6ecc2522b0d
tImIbreakdown <timifix@gmail.com>
2021-03-03 12:31:06 +0300

    s2: Define BOARD_KERNEL_IMAGE_NAME
    * located in prebuilt/Image.gz-dtb
    * fix "vendor/omni/build/tasks/kernel.mk:94: error: BOARD_KERNEL_IMAGE_NAME not defined.."

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -43,6 +43,7 @@ TARGET_USES_64_BIT_BINDER := true
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x00000100
 TARGET_PREBUILT_KERNEL := device/leeco/s2/prebuilt/Image.gz-dtb
