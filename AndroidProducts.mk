LineageOS/android_device_leeco_s2

s2: Switch from add_lunch_combo to COMMON_LUNCH_CHOICES
Bug: 77599627
Test: lunch [see entries in list]
Change-Id: Id44fcfec47eeb979a745f879031f1bec7359a633

AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2018-2020 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,3 +15,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_s2.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_s2-user \
+    lineage_s2-userdebug \
+    lineage_s2-eng

tImIbreakdown/twrp

AndroidProducts.mk
@@ -1,2 +1,7 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/omni_s2.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_s2-user \
+    omni_s2-userdebug \
+    omni_s2-eng